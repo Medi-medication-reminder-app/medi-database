ceebf33010acca689960ee3c469e90890d7c14f7
Adrian Scrab <adrian.scrab@pm.me>
2021-03-15 16:42:01 +0200

    add caretaker

--- initdb.sql
+++ initdb.sql
@@ -11,6 +11,16 @@ DROP DATABASE IF EXISTS `mediusers`;
 CREATE DATABASE `mediusers` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `mediusers`;
 
+DROP TABLE IF EXISTS `caretakers`;
+CREATE TABLE `caretakers` (
+  `caretaker_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phone_number` int NOT NULL,
+  PRIMARY KEY (`caretaker_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+TRUNCATE `caretakers`;
+
 DROP TABLE IF EXISTS `concentrations`;
 CREATE TABLE `concentrations` (
   `concentration_id` tinyint NOT NULL AUTO_INCREMENT,
@@ -25,10 +35,10 @@ CREATE TABLE `conditions` (
   `condition_id` int NOT NULL AUTO_INCREMENT,
   `condition_name` varchar(255) NOT NULL,
   `condition_details` varchar(255) NOT NULL,
-  `user_info_id` int NOT NULL,
+  `user_id` int NOT NULL,
   PRIMARY KEY (`condition_id`),
-  KEY `user_info_id` (`user_info_id`),
-  CONSTRAINT `conditions_ibfk_1` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`user_info_id`) ON DELETE CASCADE
+  KEY `user_info_id` (`user_id`),
+  CONSTRAINT `conditions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 TRUNCATE `conditions`;
@@ -64,15 +74,15 @@ TRUNCATE `frequencies`;
 DROP TABLE IF EXISTS `journal_entries`;
 CREATE TABLE `journal_entries` (
   `entry_id` int NOT NULL AUTO_INCREMENT,
-  `user_info_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `timestamp` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `feeling_id` int NOT NULL,
   `details` varchar(255) NOT NULL,
   PRIMARY KEY (`entry_id`),
-  KEY `user_info_id` (`user_info_id`),
+  KEY `user_info_id` (`user_id`),
   KEY `feeling_id` (`feeling_id`),
-  CONSTRAINT `journal_entries_ibfk_1` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`user_info_id`) ON DELETE CASCADE,
-  CONSTRAINT `journal_entries_ibfk_2` FOREIGN KEY (`feeling_id`) REFERENCES `feelings` (`feeling_id`) ON DELETE CASCADE
+  CONSTRAINT `journal_entries_ibfk_2` FOREIGN KEY (`feeling_id`) REFERENCES `feelings` (`feeling_id`) ON DELETE CASCADE,
+  CONSTRAINT `journal_entries_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 TRUNCATE `journal_entries`;
@@ -106,16 +116,16 @@ TRUNCATE `taken_times`;
 DROP TABLE IF EXISTS `taken_treatment_log`;
 CREATE TABLE `taken_treatment_log` (
   `taken_log_id` int NOT NULL AUTO_INCREMENT,
-  `user_info_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `treatment_id` int NOT NULL,
   `timestamp` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `taken_time` time NOT NULL,
   `taken` varchar(3) NOT NULL,
   PRIMARY KEY (`taken_log_id`),
-  KEY `user_info_id` (`user_info_id`),
+  KEY `user_info_id` (`user_id`),
   KEY `treatment_id` (`treatment_id`),
-  CONSTRAINT `taken_treatment_log_ibfk_1` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`user_info_id`) ON DELETE CASCADE,
-  CONSTRAINT `taken_treatment_log_ibfk_2` FOREIGN KEY (`treatment_id`) REFERENCES `treatments` (`treatment_id`)
+  CONSTRAINT `taken_treatment_log_ibfk_2` FOREIGN KEY (`treatment_id`) REFERENCES `treatments` (`treatment_id`),
+  CONSTRAINT `taken_treatment_log_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 TRUNCATE `taken_treatment_log`;
@@ -161,46 +171,49 @@ TRUNCATE `units`;
 
 DROP TABLE IF EXISTS `user_accounts`;
 CREATE TABLE `user_accounts` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `account_id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `create_date` date NOT NULL,
   `last_login` timestamp NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`account_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 TRUNCATE `user_accounts`;
-INSERT INTO `user_accounts` (`user_id`, `email`, `password`, `create_date`, `last_login`) VALUES
+INSERT INTO `user_accounts` (`account_id`, `email`, `password`, `create_date`, `last_login`) VALUES
 (1,	'gigel@email.com',	'sha512',	'2014-03-20',	'2015-03-20 21:00:00');
 
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
-  `user_info_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `account_id` int NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `gender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `birthday` date DEFAULT NULL,
-  PRIMARY KEY (`user_info_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `user_info_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user_accounts` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `caretaker_id` int DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `user_id` (`account_id`),
+  KEY `caretaker_id` (`caretaker_id`),
+  CONSTRAINT `user_info_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `user_accounts` (`account_id`),
+  CONSTRAINT `user_info_ibfk_2` FOREIGN KEY (`caretaker_id`) REFERENCES `caretakers` (`caretaker_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 TRUNCATE `user_info`;
-INSERT INTO `user_info` (`user_info_id`, `user_id`, `name`, `gender`, `birthday`) VALUES
-(1,	1,	'Gigel',	'M',	NULL);
+INSERT INTO `user_info` (`user_id`, `account_id`, `name`, `gender`, `birthday`, `caretaker_id`) VALUES
+(2,	1,	'Gigel',	'M',	NULL,	NULL);
 
 DROP TABLE IF EXISTS `user_treatment_allocation`;
 CREATE TABLE `user_treatment_allocation` (
   `user_treatment_id` int NOT NULL AUTO_INCREMENT,
-  `user_info_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `treatment_id` int NOT NULL,
   PRIMARY KEY (`user_treatment_id`),
-  KEY `user_info_id` (`user_info_id`),
+  KEY `user_info_id` (`user_id`),
   KEY `treatment_id` (`treatment_id`),
-  CONSTRAINT `user_treatment_allocation_ibfk_1` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`user_info_id`) ON DELETE CASCADE,
+  CONSTRAINT `user_treatment_allocation_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE,
   CONSTRAINT `user_treatment_allocation_ibfk_2` FOREIGN KEY (`treatment_id`) REFERENCES `treatments` (`treatment_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 TRUNCATE `user_treatment_allocation`;
 
--- 2021-03-15 11:25:14
+-- 2021-03-15 14:39:55
